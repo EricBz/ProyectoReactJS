04e0e1e8c0a0387cd708bb80fde046e032cc2a25
eric bz <soyericbenitez@gmail.com>
2022-03-24 09:17:44 -0300

    Incorporacion de icono y contador con boton.

--- src/Componentes/CartWidget/CartWidget.js
+++ src/Componentes/CartWidget/CartWidget.js
@@ -1,11 +1,18 @@
 import logo from "./logo.jpg";
 import "./CartWidget.css"
+import carrito from "./carrito.png"
 
 const CartWidget = () => {
     return (
         <div>
             <img className="logoEstilo" src={logo} alt="logo"/>
             <h1 className="titulonav">CompuStore</h1>
+            <div className="bloqueCarritoCentrado">
+            <div className="bloqueCarrito">
+                0
+                <img className="carrito" src={carrito} alt="carrito de compras"/>
+            </div>
+            </div>
         </div>
     )
 }
--- src/Componentes/ItemListContainer.js
+++ src/Componentes/ItemListContainer.js
@@ -1,6 +1,14 @@
+import ItemCount from "./ItemCount/ItemCount.js"
+
 const ItemListContainer = ({greeting}) => {
+    const onAdd = (Quantity) => console.log(Quantity)
+
+
     return (
+        <div>
+        <ItemCount initial={1} stock={25} onAdd={onAdd}/>
             <h1>{greeting}</h1>
+            </div>
     )
 }
 
